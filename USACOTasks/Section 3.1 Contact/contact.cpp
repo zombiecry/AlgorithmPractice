6dfc7c2ba4bc941412de3837626c98f531eee184
zombiecry <yangsc2013@live.com>
2014-03-11 21:47:42 -0700

    new sol(not completed)

---
+++ USACOTasks/Section 3.1 Contact/contact.cpp
@@ -0,0 +1,17 @@
+/*
+ID: yezhiyo1
+PROG: rect1
+LANG: C++
+*/
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <memory.h>
+using namespace std;
+ifstream fin("contact.in");
+ofstream fout("contact.out");
+
+int main (){
+	return 0;
+}
