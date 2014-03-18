a56dd8dde453f842aec18db807b1a53bd345d484
zombiecry <yangsc2013@live.com>
2014-03-18 15:39:52 -0700

    sol

--- USACOTasks/Section 3.2 Stringsobits/Stringsobits.cpp
+++ USACOTasks/Section 3.2 Stringsobits/Stringsobits.cpp
@@ -11,79 +11,76 @@ LANG: C++
 using namespace std;
 ifstream fin("kimbits.in");
 ofstream fout("kimbits.out");
-int N,L,I;
+unsigned int N,L,I;
 unsigned int cur=0;
-bool CheckNum(unsigned a){
-	int count=0;
-	for (int i=0;i<N;i++){
-		if(a & 0x1){
-			count++;
-			if (count>L){
-				return false;
-			}
-		}
-		a=a>>1;
-	}
-	return true;
-}
+
+
+
+
 #define MAX_N 31
-#define MAX_L 31
-int c[MAX_N+1][MAX_L+1];
+unsigned int c[MAX_N+1][MAX_N+1];
+int a[MAX_N];
 
-int main (){
-	fin>>N>>L>>I;
-
-	for (int i=1;i<=N;i++){
-		c[i][1]=i+1;
-	}
-	for (int i=1;i<=L;i++){
-		c[1][i]=2;
-	}
-	bool findFlag=false;
-	int n,l;
-	for (int i=2;i<=N;i++){
-		for (int j=2;j<=L;j++){
-			c[i][j]=c[i-1][j-1]+c[i-1][j];
-		}
-		if (c[i][L]>=I){
-			n=i;
-			l=L;
-			findFlag=true;
+void FindOnes(int n,int l,int x){
+	int pos=-1;
+	for (int i=0;i<=n;i++){
+		if (c[i][l]>=x){
+			pos=i;
 			break;
 		}
 	}
+	if (pos==0){
+		if (x==1){
+			a[N-1]=0;
+		}
+		else if (x==2){
+			a[N-1]=1;
+		}
+		else{
+			cout<<"error!\n";
+		}
+		return;
+	}
+	else{
+		a[N-1-pos]=1;
+		FindOnes(pos-1,l-1,x-c[pos-1][l]);
+	}
+}
 
-	if (!findFlag){
-		n=N;
-		l=L;
+int main (){
+	fin>>N>>L>>I;
+	for (int i=1;i<=N;i++){
+		c[i][0]=1;
 	}
-	int cur=0;
-	unsigned int maxNum=1;
-	for (int k=0;k<L-1;k++){
-		maxNum=maxNum<<1;
-		maxNum++;
+	for (int i=1;i<=L;i++){
+		c[0][i]=2;
 	}
-	for (int k=0;k<n-L;k++){
-		maxNum=maxNum<<1;
-	}
-	int count=c[n][l];
-	cur=maxNum;
-	while(count>I){
-		maxNum--;
-		if(CheckNum(maxNum)){
-			cur=maxNum;
-			count--;
+	c[0][0]=1;
+	memset(a,0,sizeof(int)*N);
+
+	if (N==1){
+		if (I==1){
+			a[N-1]=0;
+		}
+		else if (I==2){
+			a[N-1]=1;
 		}
 	}
-	std::vector <int> nums;
-	for (int k=0;k<N;k++){
-		nums.push_back(cur & 0x1);
-		cur=cur>>1;
+
+	for (int i=1;i<N;i++){
+		for (int j=1;j<=L;j++){
+			c[i][j]=c[i-1][j-1]+c[i-1][j];
+		}
+		if (c[i][L]>=I){
+			FindOnes(i,L,I);
+		}
 	}
-	for (int k=N-1;k>=0;k--){
-		fout<<nums[k];
+
+	for (int i=0;i<N;i++){
+		fout<<a[i];
 	}
 	fout<<endl;
+
 	return 0;
 
 }
