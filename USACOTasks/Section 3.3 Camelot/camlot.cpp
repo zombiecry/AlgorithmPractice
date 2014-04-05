255d31b0557e28a0c8bf67e057ee2b2e1f3355d2
zombiecry <yangsc2013@live.com>
2014-04-05 16:18:35 -0700

    sol for camlot

--- USACOTasks/Section 3.3 Camelot/camlot.cpp
+++ USACOTasks/Section 3.3 Camelot/camlot.cpp
@@ -20,7 +20,7 @@ ofstream fout("camelot.out");
 int R,C;
 const int MAX_R = 30;
 const int MAX_C = 26;
-const int PAREA = 8;
+const int PAREA = 5;
 const int INF = 0xfffffff;
 int c[MAX_R*MAX_C][MAX_R*MAX_C];
 int dist[MAX_R*MAX_C][MAX_R*MAX_C];
@@ -61,31 +61,29 @@ bool KnightConnect(int v1,int v2){
 	return false;
 }
 
-typedef pair <int,int> scPairII;
-void CalcDijDist(int src,int distArray[]){
-	std::set <scPairII> que;
+//for bfs
+int						   *distArray;
+std::vector <bool>          visited;
+
+
+void FloodFill(int src){
 	for (int i=0;i<n;i++){
 		distArray[i]=INF;
 	}
 	distArray[src]=0;
-	que.insert(scPairII(0,src));
+	visited.assign(n,false);
+	visited[src]=true;
+	std::queue <int> que;
+	que.push(src);
 	while (!que.empty()){
-		scPairII top=*que.begin();
-		int u=top.second;
-		que.erase(que.begin());
-		for (int i=0;i<n;i++){
-			if (i==u){
-				continue;
-			}
-			if (c[u][i]<INF){	//u's nei
-				int v=i;
-				int tempCost=distArray[u]+c[u][v];
-				if (tempCost<distArray[v]){
-					if (distArray[v]!=INF){
-						que.erase(que.find(scPairII(distArray[v],v)));
-					}
-					distArray[v]=tempCost;
-					que.insert(scPairII(distArray[v],v));
+		int u=que.front();
+		que.pop();
+		for (int v=0;v<n;v++){
+			if (c[u][v]<INF){
+				if (!visited[v]){
+					que.push(v);
+					distArray[v]=distArray[u]+1;
+					visited[v]=true;
 				}
 			}
 		}
@@ -124,7 +122,6 @@ int main(){
 		dist[i][i]=0;
 
 	}
-
 	for (int i=-PAREA;i<=PAREA;i++){
 		for (int j=-PAREA;j<=PAREA;j++){
 			int x=kinX+i;
@@ -136,27 +133,24 @@ int main(){
 			}
 		}
 	}
+	kingAreaNum=kingArea.size();
 	kingDist.resize(n);
 	for (int i=0;i<n;i++){
 		kingDist[i]=std::max(abs(GETX(i)-kinX),abs(GETY(i)-kinY));
 	}
-	/*
+	std::set <int> calcedPoints;
+	visited.resize(n);
 	for (int i=0;i<knightNum;i++){
-		CalcDijDist(knights[i],dist[knights[i]]);
+		distArray=dist[knights[i]];
+		FloodFill(knights[i]);
+		calcedPoints.insert(knights[i]);
 	}
 	for (int i=0;i<kingAreaNum;i++){
-		CalcDijDist(kingArea[i],dist[kingArea[i]]);
-	}
-	*/
-
-	for (int k=0;k<n;k++){
-		for (int i=0;i<n;i++){
-			for (int j=0;j<n;j++){
-				dist[i][j]=std::min(dist[i][j],dist[i][k]+dist[k][j]);
-			}
+		if (calcedPoints.count(kingArea[i])==0){
+			distArray=dist[kingArea[i]];
+			FloodFill(kingArea[i]);
 		}
 	}
-
 	knightDistSum.resize(n);
 	for (int i=0;i<n;i++){
 		int sum=0;
@@ -165,13 +159,14 @@ int main(){
 		}
 		knightDistSum[i]=sum;
 	}
-
+	int ms=INF;
 	int minSum=INF;
 	for (int i=0;i<n;i++){
 		int sum=knightDistSum[i]+kingDist[i];	//king move by itself
 		for (int j=0;j<knightNum;j++){			//king move by one knight
 			int knightIndex=knights[j];
 			int resDist=knightDistSum[i]-dist[knightIndex][i];
+			ms=std::min(resDist,ms);
 			int minMidDist=INF;
 			for (int k=0;k<kingAreaNum;k++){	//the finite king's neibor area
 				int candiIndex=kingArea[k];
