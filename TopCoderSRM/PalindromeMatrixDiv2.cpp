d27dc431bf98d8b499459e164a768a8811b3c51b
zombiecry <yangsc2013@live.com>
2014-05-22 20:50:05 -0700

    new sol

---
+++ TopCoderSRM/PalindromeMatrixDiv2.cpp
@@ -0,0 +1,94 @@
+// BEGIN CUT HERE 
+
+// END CUT HERE
+#line 5 "PalindromeMatrixDiv2.cpp"
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <deque>
+#include<queue>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+using namespace std;
+
+typedef long long int scLLi;
+typedef long long unsigned int scLLui;
+typedef std::pair <int,int> scPair2i;
+typedef std::vector <int> scVeci;
+typedef std::vector <scPair2i> scVec2i;
+typedef std::stack  <int> scStacki;
+typedef std::queue  <int> scQueuei;
+typedef std::set    <int> scSeti;
+typedef std::map<int,int> scMapii;
+typedef std::map<int,int>::iterator scMapiiIter;
+typedef std::map<int,int>::reverse_iterator scMapiiRevIter;
+#define tr(container,it)    \
+    for (it=container.begin();it!=container.end();it++)
+#define trRev(container,it)    \
+	for (it=container.rbegin();it!=container.rend();it++)
+#define scFor0(x,num) \
+	for (int x=0;x<num;x++)
+#define scFor1(x,start,num) \
+	for (int x=start;x<num;x++)
+
+int m,n;
+class PalindromeMatrixDiv2
+        { 
+        public: 
+        int minChange(vector <string> A, int rowCount, int columnCount) 
+            { 
+            //$CARETPOSITION$ 
+            } 
+        
+// BEGIN CUT HERE
+	public:
+	void run_test(int Case) { if ((Case == -1) || (Case == 0)) test_case_0(); if ((Case == -1) || (Case == 1)) test_case_1(); if ((Case == -1) || (Case == 2)) test_case_2(); if ((Case == -1) || (Case == 3)) test_case_3(); if ((Case == -1) || (Case == 4)) test_case_4(); }
+	private:
+	template <typename T> string print_array(const vector<T> &V) { ostringstream os; os << "{ "; for (typename vector<T>::const_iterator iter = V.begin(); iter != V.end(); ++iter) os << '\"' << *iter << "\","; os << " }"; return os.str(); }
+	void verify_case(int Case, const int &Expected, const int &Received) { cerr << "Test Case #" << Case << "..."; if (Expected == Received) cerr << "PASSED" << endl; else { cerr << "FAILED" << endl; cerr << "\tExpected: \"" << Expected << '\"' << endl; cerr << "\tReceived: \"" << Received << '\"' << endl; } }
+	void test_case_0() { string Arr0[] = {"0000"
+,"1000"
+,"1100"
+,"1110"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; int Arg2 = 2; int Arg3 = 1; verify_case(0, Arg3, minChange(Arg0, Arg1, Arg2)); }
+	void test_case_1() { string Arr0[] = {"0000"
+,"1000"
+,"1100"
+,"1110"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 3; int Arg2 = 2; int Arg3 = 3; verify_case(1, Arg3, minChange(Arg0, Arg1, Arg2)); }
+	void test_case_2() { string Arr0[] = {"01"
+,"10"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 1; int Arg2 = 1; int Arg3 = 1; verify_case(2, Arg3, minChange(Arg0, Arg1, Arg2)); }
+	void test_case_3() { string Arr0[] = {"1110"
+,"0001"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 0; int Arg2 = 0; int Arg3 = 0; verify_case(3, Arg3, minChange(Arg0, Arg1, Arg2)); }
+	void test_case_4() { string Arr0[] = {"01010101"
+,"01010101"
+,"01010101"
+,"01010101"
+,"01010101"
+,"01010101"
+,"01010101"
+,"01010101"}; vector <string> Arg0(Arr0, Arr0 + (sizeof(Arr0) / sizeof(Arr0[0]))); int Arg1 = 2; int Arg2 = 2; int Arg3 = 8; verify_case(4, Arg3, minChange(Arg0, Arg1, Arg2)); }
+
+// END CUT HERE
+ 
+        };
+
+    // BEGIN CUT HERE 
+    int main()
+        {
+        PalindromeMatrixDiv2 ___test; 
+        ___test.run_test(-1); 
+        system("pause");
+        } 
+    // END CUT HERE 
