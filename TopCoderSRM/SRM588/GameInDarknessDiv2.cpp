c253beab00f8b507d318a6306b8e40092ed3dd7d
zombiecry <yangsc2013@live.com>
2014-09-13 16:56:02 -0700

    new sol

--- TopCoderSRM/SRM588/GameInDarknessDiv2.cpp
+++ TopCoderSRM/SRM588/GameInDarknessDiv2.cpp
@@ -38,6 +38,7 @@ class GameInDarknessDiv2
 				m=moves.size();
 				scPair s,e;
 				scPair bp;
+				//find A and B's start
 				for (int i=0;i<n;i++) for (int j=0;j<field[i].length();j++){
 					if (field[i][j]=='A'){
 						s=scPair(i,j);
@@ -46,6 +47,10 @@ class GameInDarknessDiv2
 						bp=scPair(i,j);
 					}
 				}
+				//simulate along A's moving path, we assume dp[k][i][j] as the start of 
+				//B's turn, so we ignored the start pos of A. If A moves to B the first step 
+				//the first dp[][][] turn's start will be posA==posB, it will be identified as
+				//A win.
 				e=s;
 				for (int i=0;i<m;i++) for (int j=0;j<moves[i].length();j++){
 					switch (moves[i][j]){
@@ -67,16 +72,40 @@ class GameInDarknessDiv2
 				n=A.size();
 				m=field.size();
 				reverse(A.begin(),A.end());
+				//fill the first row, which A has 0-step left.
+				//fix the problem:
+				//since we assume currently at B's turn, if B is surrounded by A,
+				//then the place of B should also be A win. 
 				for (int j=0;j<m;j++)for (int k=0;k<field[j].length();k++){
-					dp[0][j][k]=0;
-					if (field[j][k]=='#'){dp[0][j][k]=1;}
+					dp[0][j][k]=1;
+					if (field[j][k]=='#'){continue;}					
+					if (scPair(j,k)==e){continue;}
+					int aval=0;
+					for (int dx=-1;dx<=1;dx++) for (int dy=-1;dy<=1;dy++){
+						if (dx==0 && dy==0){continue;}
+						if (dx!=0 && dy!=0){continue;}
+						int cy=j+dy;
+						int cx=k+dx;
+						if (cy <0 || cy>m-1){continue;}
+						if (cx <0 || cx>field[j].length()-1){continue;}
+						if (field[cy][cx]=='#'){continue;}
+						if (scPair(cy,cx)==e){continue;}
+						aval++;
+					}
+					if (aval){
+						dp[0][j][k]=0;
+					}
 				}
-				dp[0][e.first][e.second]=1;
+				//dp[i][j][k]: now is B's turn, A still has i step to go.
+				//and B's at (j,k)
 				for (int i=1;i<n;i++){
 					int cur=i%2;
 					int prev=(i-1)%2;
 					for (int j=0;j<m;j++)for (int k=0;k<field[j].length();k++){
 						dp[cur][j][k]=1;
+						if (cur==1 && j==0 &&k==3){
+							int a=1;
+						}
 						if (field[j][k]=='#'){continue;}
 						if (scPair(j,k)==A[i]){continue;}
 						if (j>0){
@@ -98,7 +127,7 @@ class GameInDarknessDiv2
 							if (scPair(j,k+1)!=A[i]){
 								dp[cur][j][k]&=dp[prev][j][k+1];
 							}
-						}						
+						}					
 					}
 				}
 				int res= dp[(n-1)%2][bp.first][bp.second];
@@ -173,7 +202,7 @@ class GameInDarknessDiv2
     int main()
         {
         GameInDarknessDiv2 ___test; 
-        ___test.run_test(0); 
+        ___test.run_test(5); 
         system("pause");
         } 
     // END CUT HERE 
